4485a051ac7995ef0bde121c9d07e1946a85052e
ThomasCSecurity <51128489+ThomasCSecurity@users.noreply.github.com>
2020-02-07 15:35:57 -0500

    Update start.sh

--- util/start.sh
+++ util/start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+PWD=''
+
 Sudo open -a Docker
 
 # Need to add a sleep here
 
 Sudo docker build -t capstone/web-srv:1.0 .
 
-Sudo docker run -dit --name Demo -p 8080:80 -v /Users/Ray/Desktop/3/:/usr/local/apache2/htdocs/ httpd:2.4
-
+Sudo docker run -dit --name Demo -p 8080:80 -v "$PWD":/usr/local/apache2/htdocs/ httpd:2.4
